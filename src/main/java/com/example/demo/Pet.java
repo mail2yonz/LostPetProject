9e1f9a8885d32d44babf72c544aad044dedd1e41
mail2yonz <mail2yonz@gmail.com>
2017-11-10 12:02:52 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Pet.java
@@ -0,0 +1,102 @@
+package com.example.demo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=2)
+    private String name;
+    @NotNull
+    @Size(min=2)
+    private long age;
+    @NotNull
+    @Size(min=2)
+    private String type;
+    @NotNull
+    @Size(min=2)
+    private String breed;
+    @NotNull
+    @Size(min=2)
+    private String color;
+    @NotNull
+    @Size(min=2)
+    private String feature;
+    @NotNull
+    @Size(min=2)
+    private String ownersphoneNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFeature() {
+        return feature;
+    }
+
+    public void setFeature(String feature) {
+        this.feature = feature;
+    }
+
+    public String getOwnersphoneNumber() {
+        return ownersphoneNumber;
+    }
+
+    public void setOwnersphoneNumber(String ownersphoneNumber) {
+        this.ownersphoneNumber = ownersphoneNumber;
+    }
+}
---
+++ src/main/java/com/example/demo/PetController.java
@@ -0,0 +1,80 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+
+@Controller
+public class PetController {
+
+    @Autowired
+    PetRepository petRepository;
+
+    @RequestMapping("/mainpage")
+    public String mainPage()
+    {
+        return "mainpage";
+    }
+
+    @RequestMapping("/")
+    public String listPet(Model model)
+    {
+        model.addAttribute ( "pets",petRepository.findAll () );
+
+        return "petlist";
+    }
+
+    @GetMapping("/add")
+    public String addPet(Model model)
+    {
+        model.addAttribute ( "pet",new Pet() );
+
+        return "petform";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Pet pet , BindingResult result)
+    {
+           if(result.hasErrors ())
+           {
+               return "petform";
+           }
+           petRepository.save ( pet );
+
+           return "redirect:/";
+    }
+
+    @RequestMapping("/petdetail/{id}")
+    public String showPet(@PathVariable("id") long id,Model model)
+    {
+        model.addAttribute ( "pet",petRepository.findOne ( id ) );
+
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updatePet(@PathVariable("id") long id,Model model)
+    {
+        model.addAttribute ("pet",petRepository.findOne ( id ) );
+
+        return "petform";
+    }
+
+    @RequestMapping("/delete/{id}")
+
+    public String delPet(@PathVariable("id") long id)
+    {
+        petRepository.delete ( id );
+
+        return "redirect:/";
+    }
+
+}
